727fa4cb811057d47959d68f1de1e4d790fc3b43
uselessbug <61285640+uselessbug@users.noreply.github.com>
2026-08-30 21:25:53 +0800

    experiment(frontend): register browser SAP runtime

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { installExperimentalBrowserSapRuntime } from "./apple/sap/unicornRuntime";
 import "./index.css";
 
 import "./i18n";
+
+installExperimentalBrowserSapRuntime();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
